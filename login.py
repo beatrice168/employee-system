5893e7f4676a24889611ef335aa68dca1a2e9d12
beatrice168 <helgapaul389@gmail.com>
2026-02-12 18:12:12 +0300

    added login page

---
+++ login.py
@@ -0,0 +1,40 @@
+from customtkinter import *
+from PIL import Image
+from tkinter import messagebox
+def login():
+    if usernameEntry.get() == "" or passwordEntry.get() == " " :
+        messagebox.showerror("Error", "Please fill in all fields")
+    elif usernameEntry.get() == "admin" and passwordEntry.get() == "admin":
+        messagebox.showinfo("Success", "Login successful")
+        root.destroy()
+        import ems
+    else: messagebox.showerror("Error", "Invalid username or password")                            
+
+
+
+
+root=CTk()
+
+root.geometry("930x478")
+root.resizable(False, False)
+
+root.maxsize(930, 478)
+root.minsize(930, 478)
+  
+root.title('login page')
+image= CTkImage(Image.open("backgroung.png"), size=(930, 478))
+imageLabel=CTkLabel(root, image=image,text="") 
+imageLabel.place(x=0, y=0)
+headingLabel=CTkLabel(root, text="Employee Management System", bg_color="transparent", font= ("Arial", 20, "bold"),text_color="dark blue")
+headingLabel.place(x=20, y=100)
+
+usernameEntry=CTkEntry(root, placeholder_text="Enter Your Username", width=180)
+usernameEntry.place(x=50, y=150)
+
+passwordEntry=CTkEntry(root, placeholder_text="Enter Your Password", width=180 , show="*" ,)
+passwordEntry.place(x=50, y=200)
+
+loginButton=CTkButton(root, text="Login" ,cursor="hand2", command=login)
+loginButton.place(x=70, y=250)
+
+root.mainloop()
